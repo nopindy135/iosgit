4cd613466a82d665d747a06c706abc998a1f8c94
Thitipong Sriwatthanatakul <myBoss@thitipongs-mac.local>
2016-06-08 14:18:02 +0700

    Upload Test github Version1

---
+++ HorbApp/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  HorbApp
+//
+//  Created by Thitipong Sriwatthanatakul on 8/6/59.
+//  Copyright © พ.ศ. 2559 Thitipong Sriwatthanatakul. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#define webser = @"http://192.168.137.1:9866/Service1.svc"
+
+#endif /* Header_h */
